c29b9470043d5146e12fcc696a6f3eaa39688990
GJuniarto <geryjuniarto99@gmail.com>
2023-08-31 14:38:59 +0700

    1.0

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-const aaaa = document.querySelector(".pushable")
+const buttonStart = document.querySelector(".pushable");
 
-aaaa.addEventListener("click", function() {
-    let awal = document.querySelector(".img2").style.top
-    let numb = awal.replace(/\D+/g, "")
-    let a = (-numb)
-    console.log("ss")
-    a += (-15)
-    if (a < (-480)) {
-        alert("menaang")
-    }
-    let hasil = a.toString()+"px"
-    document.querySelector(".img2").style.top = hasil
-    }
-)
+buttonStart.addEventListener('click', () => {
+    const homePage = document.querySelector('.home');
+    const gamePage = document.querySelector('.game');
+    const body = document.querySelector('body');
+    homePage.style.display = 'none';
+    gamePage.style.display = 'block';
+
+    body.addEventListener('keydown', event => {
+        if (event.keyCode === 90) {
+            let awal = document.querySelector(".img2").style.top
+            let numb = awal.replace(/\D+/g, "")
+            let a = (-numb)
+            a += (-15)
+            if (a < (-480)) {
+                alert("Red Team menaang")
+            }
+            let hasil = a.toString() + "px"
+            document.querySelector(".img2").style.top = hasil
+        }
+        if (event.keyCode === 77) {
+            let awal = document.querySelector(".img3").style.top
+            let numb = awal.replace(/\D+/g, "")
+            let a = (-numb)
+            a += (-15)
+            if (a < (-588)) {
+                alert("Blue Team menaang")
+            }
+            let hasil = a.toString() + "px"
+            document.querySelector(".img3").style.top = hasil
+        }
+    })
+
+})
+
+
+// aaaa.addEventListener("click", function() {
+//     let awal = document.querySelector(".img2").style.top
+//     let numb = awal.replace(/\D+/g, "")
+//     let a = (-numb)
+//     console.log("ss")
+//     a += (-15)
+//     if (a < (-480)) {
+//         alert("menaang")
+//     }
+//     let hasil = a.toString()+"px"
+//     document.querySelector(".img2").style.top = hasil
+//     }
+// )
